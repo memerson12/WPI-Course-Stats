b7e5136835d28911a6638080e7d7ab6a873de0c0
Michael Emerson <emerson.michael12@gmail.com>
2022-03-25 13:18:05 -0400

    Added total sample size to analysis

--- All-Reports-Anlyisis.py
+++ All-Reports-Anlyisis.py
@@ -4,6 +4,7 @@ import csv
 
 teacher_average_rating = {}
 teacher_number_classes = {}
+teacher_number_ratings = {}
 
 
 def main():
@@ -14,18 +15,21 @@ def main():
             for report in reports:
                 teacher = report['courseInfo']['instructor']
                 new_rating = report['overallRatings']['overallQuality']['average']
+                number_ratings = int(report['overallRatings']['overallQuality']['surveySize'])
                 if new_rating == '': continue
                 number_classes = teacher_number_classes.get(teacher, 0)
+                current_number_ratings = teacher_number_ratings.get(teacher, 0)
                 new_rating = float(new_rating)
                 current_rating = teacher_average_rating.get(teacher, new_rating)
                 teacher_average_rating[teacher] = (current_rating + new_rating) / 2
                 teacher_number_classes[teacher] = number_classes + 1
+                teacher_number_ratings[teacher] = current_number_ratings + number_ratings
     print(teacher_average_rating)
     with open('./results/Teacher_Raking_All_Years.csv', 'w') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow(['Professor', 'Average Rating', 'Number of Courses'])
+        writer.writerow(['Professor', 'Average Rating', 'Number of Courses', 'Total Number of Ratings'])
         for key, value in teacher_average_rating.items():
-            writer.writerow([key, value, teacher_number_classes[key]])
+            writer.writerow([key, value, teacher_number_classes[key], teacher_number_ratings[key]])
 
 
 if __name__ == '__main__':
