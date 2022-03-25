74faa4fef31340073123021bfaccf606f19864d7
Michael Emerson <emerson.michael12@gmail.com>
2022-03-25 15:44:00 -0400

    Fixed averages

--- All-Reports-Anlyisis.py
+++ All-Reports-Anlyisis.py
@@ -2,7 +2,7 @@ import os
 import json
 import csv
 
-teacher_average_rating = {}
+teacher_total_rating = {}
 teacher_number_classes = {}
 teacher_number_ratings = {}
 
@@ -20,16 +20,16 @@ def main():
                 number_classes = teacher_number_classes.get(teacher, 0)
                 current_number_ratings = teacher_number_ratings.get(teacher, 0)
                 new_rating = float(new_rating)
-                current_rating = teacher_average_rating.get(teacher, new_rating)
-                teacher_average_rating[teacher] = (current_rating + new_rating) / 2
+                current_rating = teacher_total_rating.get(teacher, 0)
+                teacher_total_rating[teacher] = (current_rating + new_rating)
                 teacher_number_classes[teacher] = number_classes + 1
                 teacher_number_ratings[teacher] = current_number_ratings + number_ratings
-    print(teacher_average_rating)
+    print(teacher_total_rating)
     with open('./results/Teacher_Raking_All_Years.csv', 'w') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(['Professor', 'Average Rating', 'Number of Courses', 'Total Number of Ratings'])
-        for key, value in teacher_average_rating.items():
-            writer.writerow([key, value, teacher_number_classes[key], teacher_number_ratings[key]])
+        for key, value in teacher_total_rating.items():
+            writer.writerow([key, value/teacher_number_classes[key], teacher_number_classes[key], teacher_number_ratings[key]])
 
 
 if __name__ == '__main__':
