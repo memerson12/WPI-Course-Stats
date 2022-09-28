098f74ff5ed4fb22c4ba84270a87f6f29346379f
Michael Emerson <emerson.michael12@gmail.com>
2022-09-28 03:18:27 -0400

    Added Database-Import.py

---
+++ Database-Import.py
@@ -0,0 +1,99 @@
+import json
+import os
+import re
+
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mydb = mysql.connector.connect(
+    host=os.getenv('PS_HOST'),
+    user=os.getenv('PS_USERNAME'),
+    password=os.getenv('PS_PASSWORD'),
+    # database="course-reports-data"
+)
+cursor = mydb.cursor()
+
+
+def main():
+    report_files = sorted(os.listdir('./data'))
+    print(report_files)
+    for report_name in report_files:
+        if report_name == '.DS_Store':
+            continue
+        inserts = []
+        year = report_name.split("_")[2].replace('.json', '')
+        if year != '2021-2022':
+            continue
+        with open(f'./data/{report_name}', 'r') as file:
+            reports = json.load(file)
+            print(f'Creating Inserts for {year}')
+            for report in reports:
+                if len(inserts) >= 1000:
+                    # print('/', '-'*100, '-/')
+                    # print(", ".join(inserts))
+                    sql = f"""
+                           INSERT INTO courses (course_title, course_number, course_type,
+                                            instructor, response_count, subject, term,
+                                            year, report_data)
+                           VALUES {", ".join(inserts)}
+                           """
+                    print(f'\tAttempting to insert {len(inserts)} courses')
+                    cursor.execute(sql)
+                    print('\t\tStatement Executed')
+                    mydb.commit()
+                    print('\t\tDatabase Committed')
+                    inserts = []
+                info = report['courseInfo']
+                # title = re.sub('[^a-zA-Z0-9& ]', '', info['courseTitle']).replace('  ', '').title().strip()
+                # title = re.sub('-', '', info['courseTitle']).replace('  ', '').title().strip()
+                title = parse_titles(info['courseTitle'])
+                # print(f'"{title}"')
+                course_number = info['courseNumber'].strip()
+                subject = course_number.split(' ')[0]
+                term = info['term'].split('_')[1]
+                if info['courseType'] not in ['Lecture', 'Web', 'Seminar', 'Practicum', 'Lab', 'ADLN', 'Corporate Ed',
+                                              'GPS', 'Conference', 'Blended', 'Other']:
+                    print(info)
+                    exit()
+                course_rating = {"overallRatings": report['overallRatings'], "frequentlyTrue": report['frequentlyTrue'],
+                                 "relativeRating": report['relativeRating'], "misc": report['misc']}
+
+                inserts.append(
+                    f"('{title}', '{course_number}', '{info['courseType']}', '{parse_titles(info['instructor'])}', "
+                    f"{info['numStudents']}, '{subject}', '{term}', '{year}', '{json.dumps(course_rating)}')")
+        if inserts:
+            sql = f"""
+            INSERT INTO courses (course_title, course_number, course_type,
+                             instructor, response_count, subject, term,
+                             year, report_data)
+            VALUES {", ".join(inserts)}
+            """
+            print(f'\tAttempting to insert {len(inserts)} courses')
+            cursor.execute(sql)
+            print('\t\tStatement Executed')
+            mydb.commit()
+            print('\t\tDatabase Committed')
+        print()
+
+
+def parse_titles(text: str):
+    # title = re.sub("'", '\'', text).replace('  ', '').title().strip()
+    title = text.replace('  ', '').replace("'", "\\'").replace('"', '\\"').title().strip()
+    title = fix_roman_numeral(title)
+    return title
+
+
+def fix_roman_numeral(text: str):
+    possible_numerals = text.split(' ')
+    search = re.search('(I{2,}|\bIV)', possible_numerals[-1], re.IGNORECASE)
+    if search:
+        # print(possible_numerals[-1], 'of', f'"{text}"', 'was a numeral')
+        return " ".join(possible_numerals[:-1]) + " " + possible_numerals[-1].upper()
+    else:
+        return " ".join(possible_numerals)
+
+
+if __name__ == '__main__':
+    main()
